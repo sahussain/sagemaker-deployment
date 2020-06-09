dbbfe27bf907afa4e5d015e3d3f0f51b844a3e9d
Ashraf Hussain <4999041+sahussain@users.noreply.github.com>
2020-06-09 14:57:21 -0400

    Update predict.py

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,9 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    # Ashraf: convert_and_pad is provided in utils.py file. 
+    #         def convert_and_pad(word_dict, sentence)
+   data_X , data_len = convert_and_pad(model.word_dict, review_to_words(input_data)) 
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +87,18 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    '''
+       To prevent tracking history (and using memory), you can also wrap the code block in with torch.no_grad():. 
+       This can be particularly helpful when evaluating a model because the model may have trainable parameters 
+       with requires_grad=True, but for which we don’t need the gradients.
+       Source:https://pytorch.org/tutorials/beginner/blitz/autograd_tutorial.html#gradients
+       
+    '''
+    
+     with torch.no_grad(): 
+            
+         output=model.forward(data)
+            
+    result=np.round(output.numpy())
 
     return result
